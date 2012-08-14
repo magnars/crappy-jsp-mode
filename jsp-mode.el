b019adead3f6b2c29dc576842fa42a034f6b716e
Magnar Sveen <magnars@gmail.com>
2012-08-14 12:13:46 +0200

    Fix <%-- --%> indentation

---
+++ jsp-mode.el
@@ -0,0 +1,32 @@
+(define-derived-mode jsp-mode
+  html-mode "JSP"
+  "Major mode for jsp.
+          \\{jsp-mode-map}"
+  (setq indent-line-function 'jsp-indent-line))
+
+(defun jsp-calculate-indent (&optional lcon)
+  (unless lcon (setq lcon (sgml-lexical-context)))
+
+  ;; Indent jsp-comment-start markers <%-- like whitespace
+  (if (and (eq (car lcon) 'tag)
+           (looking-at "--%")
+           (save-excursion (goto-char (cdr lcon)) (looking-at "<%--")))
+      (progn
+        (forward-char 4)
+        (max 0 (- (sgml-calculate-indent) 4)))
+    (sgml-calculate-indent lcon)))
+
+(defun jsp-indent-line ()
+  "Indent the current line as jsp."
+  (interactive)
+  (let* ((savep (point))
+         (indent-col
+          (save-excursion
+            (back-to-indentation)
+            (if (>= (point) savep) (setq savep nil))
+            (jsp-calculate-indent))))
+    (if (null indent-col)
+        'noindent
+      (if savep
+          (save-excursion (indent-line-to indent-col))
+        (indent-line-to indent-col)))))
